bdb13b39c4ab3de17a5a7e8e6cff098ec2f81357
Moshakis-h <moshakisloat@gmail.com>
2025-08-02 16:51:50 +0100

    ty

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -76,7 +76,7 @@ function Login() {
           </div>
         )}
         {successMessage && (
-          <div className="success-message" style={{ display: 'block' }}>
+          <div className="success-messa" style={{ display: 'block' }}>
             {successMessage}
           </div>
         )}
